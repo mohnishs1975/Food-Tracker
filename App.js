3f0d962504b8d48b550ddb227e34cd4c7652da85
mohnishs1975 <56131306+mohnishs1975@users.noreply.github.com>
2021-09-09 16:08:39 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,39 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
-import { BarCodeScanner } from 'expo-barcode-scanner';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Scanner from './screens/Scanner';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './screens/Home';
 
-export default function App() {
-  const [hasPermission, setHasPermission] = useState(null);
-  const [scanned, setScanned] = useState(false);
+import { NavigationContainer } from '@react-navigation/native';
 
-  useEffect(() => {
-    (async () => {
-      const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })();
-  }, []);
+const Stack = createStackNavigator();
 
-  const handleBarCodeScanned = ({ type, data }) => {
-    setScanned(true);
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
-  };
 
-  if (hasPermission === null) {
-    return <Text>Requesting for camera permission</Text>;
-  }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
-  }
 
+function App() {
   return (
-    <View style={styles.container}>
-      <BarCodeScanner
-        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-        style={StyleSheet.absoluteFillObject}
-      />
-      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Scanner" component={Scanner} />
+    </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({ ... }); 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default () => {
+  return (
+    <NavigationContainer>
+     
+        <App />
+      
+    </NavigationContainer>
+  )
+}
