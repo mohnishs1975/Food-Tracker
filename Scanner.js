55f18001c4ab7355368b0b3b39d37003328fb816
TingruLian <90277008+TingruLian@users.noreply.github.com>
2021-09-16 17:51:36 -0400

    Add files via upload

---
+++ Scanner.js
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+
+export default function Scanner( {navigation} ) {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    str = 'https://api.nal.usdCCa.gov/fdc/v1/foods/search?api_key=DEMO_KEY&query=' + id;
+    console.log(str);
+    writeUserData(id, name, calories)
+  };
+  const name = () => {
+    return fetch(str)
+      .then((response) => response.json())
+      .then((json) => {
+        return json.description;
+      })
+  }  
+  
+  const calories = () => {
+    return fetch(str)
+      .then((response) => response.json())
+      .then((json) => {
+        return json.nutrientNumber;
+      })
+  }
+  function writeUserData(id, name, calories) {
+    firebase.database().ref('users/' + userId).set({
+      name: name,
+      calories: calories
+    });
+  }
+
+
+
+
+  const calories = fda_api;
+  const TextInANest = () => {
+    const [titleText, setTitleText] = useState("Calories:");
+    const bodyText = calories;
+  
+  
+    return (
+      <Text style={styles.baseText}>
+        <Text style={styles.titleText} onPress={onPressTitle}>
+          {titleText}
+          {"\n"}
+          {"\n"}
+        </Text>
+        <Text numberOfLines={5}>{bodyText}</Text>
+      </Text>
+    );
+  };
+  
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+});
